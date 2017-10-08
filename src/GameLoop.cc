1ce869bc9e26dac6d8d1919af5c284c20246bbd5
David Molnar <decesso@Davids-MacBook-Air.local>
2017-10-09 00:53:20 +0200

    Fix two continously pressed key issue #3

--- src/GameLoop.cc
+++ src/GameLoop.cc
@@ -10,8 +10,8 @@ GameLoop::GameLoop(Logic& logic1, Logic& logic2, View& view, Character& characte
 , view_(view)
 , run_(true)
 {
-	KeyMap km1{SDLK_LEFT, SDLK_RIGHT, SDLK_UP, SDLK_DOWN, SDLK_SPACE};
-	KeyMap km2{SDLK_a, SDLK_d, SDLK_w, SDLK_s, SDLK_LSHIFT}; //TODO get from parameter
+	KeyMap km1{SDLK_LEFT, SDLK_RIGHT, SDLK_UP, SDL_SCANCODE_DOWN, SDLK_SPACE};
+	KeyMap km2{SDLK_a, SDLK_d, SDLK_w, SDL_SCANCODE_S, SDLK_LSHIFT}; //TODO get from parameter
 	controlPlayer1_ = std::make_unique<LocalController>(logicPlayer1_, character1, km1);
 	if (type == GameType::AI) {
 		controlPlayer2_ = std::make_unique<Ai>(logicPlayer2_, character2);
