fc184b5a3912d7628635fdc7d03bc0d98a15d3b1
Molnár Dávid <dawemolnar@gmail.com>
2017-11-02 14:09:33 +0100

    Add character selection function to main

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,21 @@
 #include <time.h>
 #include <tclap/CmdLine.h>
 
+std::shared_ptr<Character>
+makeCharacter(char type, Logic& self, Logic& enemy)
+{
+	switch(type) {
+		case 'n':
+			return std::make_shared<BetaCharacter>(self,enemy);
+		case 'm':
+			return std::make_shared<BetaCharacter>(self,enemy);
+		case 'w':
+			return std::make_shared<BetaCharacter>(self,enemy);
+		default:
+			return std::make_shared<BetaCharacter>(self,enemy);
+	}
+}
+
 int main(int argc, char *argv[])
 {
 	srand((unsigned)time(NULL));
@@ -16,7 +31,7 @@ int main(int argc, char *argv[])
 		TCLAP::ValueArg<char> char2("c2", "character-2", "Player 1 character", false, 'm', "name", cmd);
 		TCLAP::SwitchArg localgame("l", "local-game", "Local multiplayer", cmd, false);
 		TCLAP::SwitchArg aigame("a", "ai-game", "Against AI", cmd, false);
-		TCLAP::ValueArg<std::string> networkgame("n", "enemy-addr", "Netwrok game enemy address", false, {"tcp://127.0.0.1:4242"}, "address", cmd);
+		TCLAP::ValueArg<std::string> networkgame("n", "enemy-addr", "Network game enemy address", false, {"tcp://127.0.0.1:4242"}, "address", cmd);
 		
 		cmd.parse(argc, argv);
 		
@@ -24,10 +39,10 @@ int main(int argc, char *argv[])
 		Logic player2;
 		player1.setEnemy(player2);
 		player2.setEnemy(player1);
-		BetaCharacter character1(player1, player2);
-		BetaCharacter character2(player2, player1);
+		std::shared_ptr<Character> character1 = makeCharacter(char1.getValue(), player1, player2);
+		std::shared_ptr<Character> character2 = makeCharacter(char2.getValue(), player2, player1);
 		std::shared_ptr<View> view = std::make_shared<View>(player1, player2, character1, character2);
-		GameLoop game(player1, player2, view, character1, character2, GameType::AI);
+		GameLoop game(player1, player2, view, character1.get(), character2.get(), GameType::AI);
 		game.loop();
 	} catch (const std::exception& e) {
 		std::cerr << "Exception: " << e.what() << std::endl;
