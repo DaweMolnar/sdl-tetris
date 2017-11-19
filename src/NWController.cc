b887754b6bc29802ec9d38c68073563d6c98c3fa
David Molnar <5827217+Decesso@users.noreply.github.com>
2017-11-19 01:51:56 +0100

    Add source file for network controller, and handle keys in server

---
+++ src/NWController.cc
@@ -0,0 +1,30 @@
+#include "NWController.hh"
+
+void
+NWSController::handleKey(const SDL_Keycode& key)
+{
+	if (key == keyMap_.left) {
+		logic_->move(-1, 0);
+	} else if (key == keyMap_.right) {
+		logic_->move(1, 0);
+	} else if (key == keyMap_.up) {
+		logic_->rotate();
+	} else if (key == keyMap_.special) {
+		character_.doSpecial();
+		//TODO send special
+	}
+}
+
+void
+NWSController::tick()
+{
+	//TODO send mana and table and gameover
+	const Uint8* keystate = SDL_GetKeyboardState(nullptr);
+	if ( keystate[keyMap_.down] ) logic_->move(0, 1);
+}
+
+void
+NWCController::tick()
+{
+	//TODO receive, and parse messages, send plusLines from logic
+}
--- src/NWController.hh
+++ src/NWController.hh
@@ -2,17 +2,18 @@
 
 #include "ControllerInterface.hh"
 #include "Client.hh"
+#include "KeyMap.hh"
 
 class NWCController : public ControllerInterface
 {
 public:
-	NWCController(Logic& logic, Character& character, std::shared_ptr<TcpClient> client)
+	NWCController(std::shared_ptr<LogicInterface> logic, Character& character, std::shared_ptr<TcpClient> client)
 	: ControllerInterface(logic, character)
 	, client_(client)
 	{}
 
-	void handleKey(const SDL_Keycode& key) override {}
-	void tick() override {}
+	void handleKey(const SDL_Keycode&) override {}
+	void tick() override;
 
 private:
 	std::shared_ptr<TcpClient> client_;
@@ -21,14 +22,16 @@ private:
 class NWSController : public ControllerInterface
 {
 public:
-	NWSController(Logic& logic, Character& character, std::shared_ptr<TcpClient> client)
+	NWSController(std::shared_ptr<LogicInterface> logic, Character& character, std::shared_ptr<TcpClient> client, KeyMap& keyMap)
 	: ControllerInterface(logic, character)
+	, keyMap_(keyMap)
 	, client_(client)
 	{}
 
-	void handleKey(const SDL_Keycode& key) override {}
-	void tick() override {}
+	void handleKey(const SDL_Keycode&) override;
+	void tick() override;
 
 private:
+	KeyMap keyMap_;
 	std::shared_ptr<TcpClient> client_;
 };
