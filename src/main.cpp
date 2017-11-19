df9320569453567c824bd395abc0b837fc63a7ba
David Molnar <5827217+Decesso@users.noreply.github.com>
2017-11-19 16:11:52 +0100

    Add second character from network feature

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,16 @@
 #include <time.h>
 #include <tclap/CmdLine.h>
 
+std::vector<std::string> split(const std::string &s, char delim) {
+  std::stringstream ss(s);
+  std::string item;
+  std::vector<std::string> elems;
+  while (std::getline(ss, item, delim)) {
+    elems.push_back(std::move(item));
+  }
+  return elems;
+}
+
 std::shared_ptr<Character>
 makeCharacter(char type, std::shared_ptr<LogicInterface> self, std::shared_ptr<LogicInterface> enemy)
 {
@@ -23,13 +33,31 @@ makeCharacter(char type, std::shared_ptr<LogicInterface> self, std::shared_ptr<L
 	}
 }
 
+char
+getNWCharacter(std::shared_ptr<TcpClient> client)
+{
+	std::string message = client->receive(5);
+	if (message.empty()) throw std::runtime_error("Enemy character did not received in time");
+	auto msg = split(message, '*');
+	if (msg.size() < 3 || msg.at(1) != "C" || msg.at(2).size() != 1) throw std::runtime_error("Bad message format when receiving message from client");
+	return msg.at(2).at(0);
+}
+
+void
+sendLocalCharacter(char character, std::shared_ptr<TcpClient> client)
+{
+  std::ostringstream ss;
+	ss << "*C*" << character << "*#";
+	client->send(ss.str());
+}
+
 int main(int argc, char *argv[])
 {
 	srand((unsigned)time(NULL));
 	try {
 		TCLAP::CmdLine cmd("Multiplayer tetris", ' ', "0.1.1");
 		TCLAP::ValueArg<char> char1("1", "character-1", "Player 1 character", true, 'n', "name", cmd);
-		TCLAP::ValueArg<char> char2("2", "character-2", "Player 1 character", true, 'm', "name", cmd);
+		TCLAP::ValueArg<char> char2("2", "character-2", "Player 2 character", false, ' ', "name", cmd);
 		TCLAP::SwitchArg localgame("l", "local-game", "Local multiplayer", cmd, false);
 		TCLAP::SwitchArg aigame("a", "ai-game", "Against AI", cmd, false);
 		TCLAP::ValueArg<std::string> networkgame("n", "enemy-addr", "Network game enemy address", false, {"tcp://127.0.0.1:4242"}, "address", cmd);
@@ -41,12 +69,15 @@ int main(int argc, char *argv[])
 
 		std::shared_ptr<TcpClient> client;
 		std::string address = networkgame.getValue();
-
+		char charStr1 = char1.getValue();
+		char charStr2;
 		GameType type;
 		if (localgame.getValue()) {
+			charStr2 = char2.getValue();
 			player2 = std::make_shared<Logic>();
 			type = GameType::TWOPLAYER;
 		}else if (aigame.getValue()) {
+			charStr2 = char2.getValue();
 			player2 = std::make_shared<Logic>();
 			type = GameType::AI;
 		}else if (!address.empty()) {
@@ -58,6 +89,8 @@ int main(int argc, char *argv[])
 			std::string message = client->receive(5);
 			if (message.empty()) throw std::runtime_error("Did not find any opponent");
 			if (message != "start") throw std::runtime_error("Invalid game start message received");
+			sendLocalCharacter(charStr1, client);
+			charStr2 = getNWCharacter(client);
 			player2 = std::make_shared<PassiveLogic>();
 			type = GameType::NETWORK;
 		} else {
@@ -67,8 +100,8 @@ int main(int argc, char *argv[])
 		player1->setEnemy(player2);
 		player2->setEnemy(player1);
 
-		std::shared_ptr<Character> character1 = makeCharacter(char1.getValue(), player1, player2);
-		std::shared_ptr<Character> character2 = makeCharacter(char2.getValue(), player2, player1);
+		std::shared_ptr<Character> character1 = makeCharacter(charStr1, player1, player2);
+		std::shared_ptr<Character> character2 = makeCharacter(charStr2, player2, player1);
 		std::shared_ptr<View> view = std::make_shared<View>(player1, player2, *character1, *character2);
 
 		GameLoop game(player1, player2, view, *character1, *character2, type, client);
